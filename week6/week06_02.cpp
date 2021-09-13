6ff78a98d7fe1f243cf83ffd160ee2a960848498
misterjimza <you@example.com>
2021-09-13 17:58:36 +0700

    M

--- week6/week06_02.cpp
+++ week6/week06_02.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include<cmath>
+#include <cmath>
 #include <algorithm>
 using namespace std;
 // รับจำนวนเต็ม 2 จำนวนแล้ง แสดงผลจำนวนเฉพาะ ที่มีค่าใรช่วงของตัวเลขที่รับเข้ามา
@@ -10,16 +10,16 @@ int main()
     cin >> a >> b;
     cout << "Prime Numbers in the range of " << min(a,b) <<  " to " << max(a,b) << " are ";
 
-    for(int i = min(a,b); i <= max(a,b); ++i) //loop to check for each number in the range
+    for(int i = min(a,b); i <= max(a,b); ++i) 
     {   
-        int ctr=0; //to maintain factor count
-        for(int j = 2 ; j <= sqrt(i); ++j) //checking for factors
+        int ctr=0; 
+        for(int j = 2 ; j <= sqrt(i); ++j) 
         {   if(i%j==0)
-                ctr=1; //increasing factor count when found
+                ctr=1; 
         }
         if (i != 1)
         {
-            if(ctr==0) //checking and printing prime numbers
+            if(ctr==0) 
                 cout<<i<<" ";
         }
         
